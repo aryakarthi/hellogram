e9918094166c00a6463d8daa09c83ed4e6e2e78d
aryakarthi <aryaperumal.m@gmail.com>
2024-04-15 16:13:59 +0530

    updated

--- php/users.php
+++ php/users.php
@@ -8,7 +8,7 @@
   $sql = mysqli_query($conn,"SELECT * FROM users WHERE NOT unique_id = {$sent_id}");
   $output = "";
 
-  if(mysqli_num_rows($sql) == 1){
+  if(mysqli_num_rows($sql) == 0){
     $output .= "No users found!";
   }elseif(mysqli_num_rows($sql) > 0){
     include "data.php"; 
